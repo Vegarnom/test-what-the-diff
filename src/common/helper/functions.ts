340f8d317bc3b98fb5c73834c522de1be4381560
Ngoc Pham <ngoc-phd@genkisystem.com>
2023-06-26 11:38:25 +0700

    add func

--- src/common/helper/functions.ts
+++ src/common/helper/functions.ts
@@ -5,3 +5,18 @@ export const checkAndRemoveSpaceInput = (input: string): boolean => {
   }
   return true;
 };
+ export const checkSpecialCharacter = (input: string): boolean => {
+  const regex = /^[a-zA-Z0-9]*$/;
+  if (regex.test(input)) {
+    return true;
+  }
+  return false;
+ }
+
+ export const checkSpecialCharacterAndSpace = (input: string): boolean => {
+  const regex = /^[a-zA-Z0-9 ]*$/;
+  if (regex.test(input)) {
+    return true;
+  }
+  return false;
+ }
