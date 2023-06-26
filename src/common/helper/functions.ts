3841e5ea39ac8afb8aead98142e1bd5990b7c36a
Ngoc Pham <ngoc-phd@genkisystem.com>
2023-06-26 12:15:03 +0700

    check email

--- src/common/helper/functions.ts
+++ src/common/helper/functions.ts
@@ -1,3 +1,5 @@
+import { checkEmailAndRemove } from './functions';
+import { checkAndRemoveSpaceInput } from 'src/common/helper/functions';
 export const checkAndRemoveSpaceInput = (input: string): boolean => {
   input = input.trim();
   if (input === '') {
@@ -20,3 +22,16 @@ export const checkAndRemoveSpaceInput = (input: string): boolean => {
   }
   return false;
  }
+
+ export const checkEmailAndRemoveSpace = (input: string): boolean => {
+  input = input.trim();
+  if (input === '') {
+    return false;
+  }
+  const regex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
+  if (regex.test(input)) {
+    return true;
+  }
+  return false;
+ }
+
